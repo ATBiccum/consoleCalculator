c518556020f095e418f95a4be5af1135b7e93b81
Anthony Biccum <atbiccum@gmail.com>
2021-09-16 12:31:42 -0700

    Switch case done, need to fix variable types and implement loop. Possibly do an input counter

--- consoleCalculator/Program.cs
+++ consoleCalculator/Program.cs
@@ -19,7 +19,7 @@ namespace consoleCalculator
     {
         static void Main(string[] args)
         {
-            Console.Write("Welcome to Console Calculator! What kind of operation do you with to perform?");
+            Console.WriteLine("Welcome to Console Calculator! What kind of operation do you with to perform?");
             Console.WriteLine("a - Addition");
             Console.WriteLine("s - Subtraction");
             Console.WriteLine("m - Multiplication");
@@ -33,8 +33,32 @@ namespace consoleCalculator
                     int input1 = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("Please enter your second number:");
                     int input2 = Convert.ToInt32(Console.ReadLine());
-                    int answer = input1 + input2;
-                    Console.WriteLine($"Your Result: {answer}");    
+                    int answer1 = input1 + input2;
+                    Console.WriteLine($"Your Result: {answer1}");    
+                    break;
+                case "s":
+                    Console.WriteLine("Please enter your first number:");
+                    int input3 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please enter your second number:");
+                    int input4 = Convert.ToInt32(Console.ReadLine());
+                    int answer2 = input3 - input4;
+                    Console.WriteLine($"Your Result: {answer2}");
+                    break;
+                case "m":
+                    Console.WriteLine("Please enter your first number:");
+                    int input5 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please enter your second number:");
+                    int input6 = Convert.ToInt32(Console.ReadLine());
+                    int answer3 = input5 * input6;
+                    Console.WriteLine($"Your Result: {answer3}");
+                    break;
+                case "d":
+                    Console.WriteLine("Please enter your first number:");
+                    var input7 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Please enter your second number:");
+                    var input8 = Convert.ToDouble(Console.ReadLine());
+                    double answer4 = input7 / input8;
+                    Console.WriteLine($"Your Result: {answer4}.2d");
                     break;
             }
         }
