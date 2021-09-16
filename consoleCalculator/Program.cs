c9024f0bf7c84f78271390ce66626a4bbb381fc6
Anthony Biccum <atbiccum@gmail.com>
2021-09-16 14:20:48 -0700

    Added second calculator class. Doing switch case inside of program class and sending calculations to calculator

--- consoleCalculator/Program.cs
+++ consoleCalculator/Program.cs
@@ -19,54 +19,71 @@ namespace consoleCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Console Calculator! What kind of operation do you with to perform?");
-            Console.WriteLine("a - Addition");
-            Console.WriteLine("s - Subtraction");
-            Console.WriteLine("m - Multiplication");
-            Console.WriteLine("d - Division");
+            bool endApp = false; //Bool value to determine if we are running the program or not
 
-
-            switch(Console.ReadLine())
+            while(!endApp)
             {
-                case "a":
-                    Console.WriteLine("Please enter your first number:");
-                    int input1 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Please enter your second number:");
-                    int input2 = Convert.ToInt32(Console.ReadLine());
-                    int answer1 = input1 + input2;
-                    Console.WriteLine($"Your Result: {answer1}");    
-                    break;
-                case "s":
-                    Console.WriteLine("Please enter your first number:");
-                    int input3 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Please enter your second number:");
-                    int input4 = Convert.ToInt32(Console.ReadLine());
-                    int answer2 = input3 - input4;
-                    Console.WriteLine($"Your Result: {answer2}");
-                    break;
-                case "m":
-                    Console.WriteLine("Please enter your first number:");
-                    int input5 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Please enter your second number:");
-                    int input6 = Convert.ToInt32(Console.ReadLine());
-                    int answer3 = input5 * input6;
-                    Console.WriteLine($"Your Result: {answer3}");
-                    break;
-                case "d":
-                    Console.WriteLine("Please enter your first number:");
-                    var input7 = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("Please enter your second non-zero number:");
-                    
-                    var input8 = Convert.ToDouble(Console.ReadLine());
-                    while(input8 == 0)
-                    {
-                        Console.WriteLine("Cannot divide by zero! Enter second number: ");
-                        input8 = Convert.ToDouble(Console.ReadLine());
-                    }
-                    double answer4 = input7 / input8;
-                    Console.WriteLine($"Your Result: {answer4}");
-                    break;
+                Console.WriteLine("Welcome to Console Calculator! What kind of operation do you with to perform?");
+                Console.WriteLine("a - Addition");
+                Console.WriteLine("s - Subtraction");
+                Console.WriteLine("m - Multiplication");
+                Console.WriteLine("d - Division");
+                
+                switch(Console.ReadLine())
+                {
+                    case "a":
+                        
+                        Calculator.doAddition(addInput1, addInput2);
+                        break;
+                    case "s":
+
+                        break;
+                    case "m":
+
+                        break;
+                    case "d":
+
+                        while(input8 == 0)
+                        {
+                            Console.WriteLine("Cannot divide by zero! Enter second number: ");
+                            input8 = Convert.ToDouble(Console.ReadLine());
+                        }
+                        double answer4 = input7 / input8;
+                        Console.WriteLine($"Your Result: {answer4}");
+                        break;
+                }
             }
+            return;
         }
+    }
+    class Calculator
+    {
+        public static double doAddition(double input1, double input2)
+        {
+            double answer = double.NaN;
+
+            return answer;
+        }
+        public static double doSubtraction(double input1, double input2)
+        {
+            double answer = double.NaN;
+
+            return answer;
+        }
+
+        public static double doMultiplication(double input1, double input2)
+        {
+            double answer = double.NaN;
+
+            return answer;
+        }
+        public static double doDivision(double input1, double input2)
+        {
+            double answer = double.NaN;
+
+            return answer;
+        }
+
+
     }
 }
